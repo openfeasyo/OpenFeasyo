4d3c0efea0e7bc4e222f26911b258d57bfca6b90
lomelina_etro <lomelina@etro.vub.ac.be>
2020-04-04 14:47:20 +0200

    Windows project file of the OpenFeasyo.Platform

---
+++ src/OpenFeasyo.Platform/Platforms/Windows/Data/RegistryElements.cs
@@ -0,0 +1,15 @@
+﻿
+namespace OpenFeasyo.Platform.Data
+{
+    public class RegistryElements
+    {
+        public const string REGISTRY_ROOT_SECTION = @"HKEY_CURRENT_USER\SOFTWARE\ict4rehab";
+        internal const string REGISTRY_SECTION = @"HKEY_CURRENT_USER\SOFTWARE\ict4rehab\PatientsFor";
+        internal const string REGISTRY_THERAPIST_TAG = "Current";
+        
+        public const string REGISTRY_FORCE_KINECT_1 = "ForceKinect1";
+
+        internal const string REGISTRY_SERVER = @"Server";
+        internal const string REGISTRY_ALLOW_ALL_CERTIFICATES = @"AllowAllCertificates";
+    }
+}
---
+++ src/OpenFeasyo.Platform/Platforms/Windows/Platform/UIThread.cs
@@ -0,0 +1,66 @@
+﻿#if ANDROID
+    using Android.App;
+    using Android.Widget;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenFeasyo.Platform.Platform
+{
+    public abstract class UIThread
+    {
+        private static UIThread _uithread = null;
+ 
+        public static UIThread Instance
+        {
+            get { return _uithread; }
+            set { _uithread = value; }
+        }
+
+        public static void Invoke(Action a)
+        {
+            if (_uithread != null)
+            {
+                _uithread.InternalInvoke(a);
+            }
+        }
+
+        public static void ShowMessage(String title, String message)
+        {
+            if (_uithread != null)
+            {
+                _uithread.InternalShowMessage(title,message);
+            }
+        }
+
+        internal abstract void InternalInvoke(Action a);
+
+        internal abstract void InternalShowMessage(String title, String message);
+    }
+
+#if ANDROID
+    public class AndroidUIThread : UIThread
+    {
+        private Activity _activity;
+        public AndroidUIThread(Activity activity) {
+            _activity = activity;
+        }
+
+        internal override void InternalInvoke(Action a)
+        {
+            _activity.RunOnUiThread(a);
+        }
+
+        internal override void InternalShowMessage(string title, string message)
+        {
+            _activity.RunOnUiThread(() => {
+                //Toast.MakeText(_activity, message, ToastLength.Long).Show();
+                Console.WriteLine(message);
+            }); 
+        }
+    }
+#endif
+}
