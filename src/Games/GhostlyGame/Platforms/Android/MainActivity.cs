58a02e27dd9e875b41f14af6b31e907db9b86b6e
lomelina_etro <lomelina@etro.vub.ac.be>
2020-04-06 14:39:43 +0200

    Initial commit of the Ghostly game project files for Android and Windows

---
+++ src/Games/GhostlyGame/Platforms/Android/MainActivity.cs
@@ -0,0 +1,120 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Content.PM;
+using Android.Views;
+using System;
+using Android;
+using OpenFeasyo.Platform.Platform;
+using OpenFeasyo.Platform.Data;
+using OpenFeasyo.Platform.Controls.Drivers;
+using OpenFeasyo.Platform.Controls.Analysis;
+using System.Collections.Generic;
+
+namespace GhostlyLib
+{
+    [Activity(
+         Label = "@string/app_name",
+         Theme = "@style/AppTheme",
+         ScreenOrientation = ScreenOrientation.SensorLandscape,
+         ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden,
+         MainLauncher = true)]
+    public class MainActivity : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        public static MainActivity Instance { get; private set; }
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Instance = this;
+
+            EnableImmersiveMode();
+            this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+            this.Window.AddFlags(WindowManagerFlags.KeepScreenOn);
+            base.OnCreate(savedInstanceState);
+
+            InputDeviceManager.Instance = new StaticDriverManager();
+            InputAnalyzerManager.Instance = new StaticAnalysisManager();
+            SeriousGames.CurrentGame = new ConfiguredGame();
+            SeriousGames.CurrentGame.Name = "Ghostly";
+            SeriousGames.HomeLocation = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath;
+            
+            UIThread.Instance = new AndroidUIThread(this);
+
+            this.RequestPermissions(new string[] {
+                        Manifest.Permission.WakeLock,
+                        Manifest.Permission.ReadExternalStorage,
+                        Manifest.Permission.WriteExternalStorage
+            }, 1);
+
+            var g = new GhostlyGame();
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+
+
+
+        public void EnableImmersiveMode()
+        {
+            int uiOptions = (int)this.Window.DecorView.SystemUiVisibility;
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+            this.Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+        }
+
+
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            this.Finish();
+            this.FinishAffinity();
+            // Ugly ugly ugly. This is done to cleanup the Delsys pipeline. Without the following line, it is going to crash every second start
+            //Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+        }
+
+
+        private Dictionary<int, Action<bool>> permissionRequests = new Dictionary<int, Action<bool>>();
+
+        public void RequestPermissions(string[] permissions, int requestCode, Action<bool> action)
+        {
+            List<string> neededPermissions = new List<string>();
+            foreach (string permission in permissions)
+            {
+                if (CheckSelfPermission(permission) != Permission.Granted)
+                {
+                    neededPermissions.Add(permission);
+                }
+            }
+            if (neededPermissions.Count > 0)
+            {
+                RequestPermissions(neededPermissions.ToArray(), 1);
+                permissionRequests.Add(requestCode, action);
+            }
+            else
+            {
+                action.Invoke(true);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (permissionRequests.ContainsKey(requestCode))
+            {
+                bool granted = true;
+                foreach (Permission p in grantResults)
+                {
+                    granted = granted && p == Permission.Granted;
+                }
+
+                Action<bool> action = permissionRequests[requestCode];
+                permissionRequests.Remove(requestCode);
+                action.Invoke(granted);
+            }
+        }
+        
+    }
+}
---
+++ src/Games/GhostlyGame/Platforms/Android/StaticDriverManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using OpenFeasyo.Platform.Controls.Drivers;
+
+// references to linked drivers
+using TrignoEmg;
+
+namespace GhostlyLib
+{
+    public class StaticDriverManager : IDriverManager
+    {
+        ObservableCollection<IDevice> _drivers = new ObservableCollection<IDevice>();
+
+        internal StaticDriverManager()
+        {
+            _drivers.Add(new TrignoEmgDevice());
+        }
+
+        public ObservableCollection<IDevice> Drivers
+        {
+            get { return _drivers; }
+        }
+
+        public void UnloadAll()
+        {
+            foreach (IDevice d in Drivers)
+            {
+                if (d.IsLoaded)
+                {
+                    d.UnloadDriver();
+                }
+            }
+        }
+    }
+}
