af64a05c60a8b0cc935e9c4ad10a9c7616b1c46d
lomelina_etro <lomelina@etro.vub.ac.be>
2020-07-06 14:25:41 +0200

    Windows project file for the Loop game

---
+++ src/Games/LoopGame/Platforms/Windows/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * The program is developed as a data collection tool in the fields of motion 
+ * analysis and physical condition.The user of the software is motivated to 
+ * complete exercises through the use of Games. This program is available as
+ * a part of the open source project OpenFeasyo found at
+ * https://github.com/openfeasyo/OpenFeasyo>.
+ * 
+ * Copyright (c) 2020 - Lubos Omelina
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License version 3 as published 
+ * by the Free Software Foundation. The Software Source Code is submitted 
+ * within i-DEPOT holding reference number: 122388.
+ */
+using System;
+
+namespace LoopLib
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new LoopGame())
+                game.Run();
+        }
+    }
+#endif
+}
