0f3aacf7f684180d35cac4578d91e42e996319b4
lomelina_etro <lomelina@etro.vub.ac.be>
2024-02-01 13:40:25 +0100

    Added details Android build files to the loop game

---
+++ src/Games/LoopGame/Platforms/Android/MainActivity.cs
@@ -0,0 +1,118 @@
+﻿using Android;
+using Android.App;
+using Android.OS;
+using Android.Content.PM;
+using Android.Views;
+using OpenFeasyo.Platform.Controls.Drivers;
+using OpenFeasyo.Platform.Controls.Analysis;
+using OpenFeasyo.Platform.Data;
+using OpenFeasyo.Platform.Platform;
+using System.Collections.Generic;
+using System;
+using Android.Runtime;
+
+namespace LoopLib
+{
+    [Activity(
+        Label = "@string/app_name", 
+        Theme = "@style/AppTheme",
+        ScreenOrientation = ScreenOrientation.SensorLandscape,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden,
+        MainLauncher = true)]
+    public class MainActivity : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        public static MainActivity Instance { get; private set; }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+            this.Window.AddFlags(WindowManagerFlags.KeepScreenOn);
+
+
+            base.OnCreate(savedInstanceState);
+            EnableImmersiveMode();
+
+            Instance = this;
+
+
+
+            InputDeviceManager.Instance = new StaticDriverManager();
+            InputAnalyzerManager.Instance = new StaticAnalysisManager();
+            SeriousGames.CurrentGame = new ConfiguredGame();
+            SeriousGames.CurrentGame.Name = "Loop";
+            SeriousGames.HomeLocation = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath;
+
+            UIThread.Instance = new AndroidUIThread(this);
+
+
+            if (Android.OS.Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                // TODO check whether we need to show permission rationalle
+                this.RequestPermissions(new string[] {
+                            Manifest.Permission.WakeLock,
+                            Manifest.Permission.ReadExternalStorage,
+                            Manifest.Permission.WriteExternalStorage
+                }, 1);
+            }
+
+            var g = new LoopGame();
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+
+        public void EnableImmersiveMode()
+        {
+            int uiOptions = (int)this.Window.DecorView.SystemUiVisibility;
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+            this.Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+        }
+
+        private Dictionary<int, Action<bool>> permissionRequests = new Dictionary<int, Action<bool>>();
+
+        public void RequestPermissions(string[] permissions, int requestCode, Action<bool> action)
+        {
+            // On older devices, permisisons are always granted while the app installation
+            if (Android.OS.Build.VERSION.SdkInt < BuildVersionCodes.M)
+            {
+                action.Invoke(true);
+                return;
+            }
+
+            List<string> neededPermissions = new List<string>();
+            foreach (string permission in permissions)
+            {
+                if (CheckSelfPermission(permission) != Permission.Granted)
+                {
+                    neededPermissions.Add(permission);
+                }
+            }
+            if (neededPermissions.Count > 0)
+            {
+                RequestPermissions(neededPermissions.ToArray(), requestCode);
+                permissionRequests.Add(requestCode, action);
+            }
+            else
+            {
+                action.Invoke(true);
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (permissionRequests.ContainsKey(requestCode))
+            {
+                bool granted = true;
+                foreach (Permission p in grantResults)
+                {
+                    granted = granted && p == Permission.Granted;
+                }
+
+                Action<bool> action = permissionRequests[requestCode];
+                permissionRequests.Remove(requestCode);
+                action.Invoke(granted);
+            }
+        }
+    }
+}
---
+++ src/Games/LoopGame/Platforms/Android/StaticDriverManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using OpenFeasyo.Platform.Controls.Drivers;
+using TrignoEmg;
+
+namespace LoopLib
+{
+    public class StaticDriverManager : IDriverManager
+    {
+        ObservableCollection<IDevice> _drivers = new ObservableCollection<IDevice>();
+
+        internal StaticDriverManager()
+        {
+            _drivers.Add(new TrignoEmgDevice());
+        }
+
+        public ObservableCollection<IDevice> Drivers
+        {
+            get { return _drivers; }
+        }
+
+        public void UnloadAll()
+        {
+            foreach (IDevice d in Drivers)
+            {
+                if (d.IsLoaded)
+                {
+                    d.UnloadDriver();
+                }
+            }
+        }
+    }
+}
