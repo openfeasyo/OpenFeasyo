8902b9650c6b8b1e5a80f3c185b7f9c0e461f4b1
kkostkova <kkostkova@etrovub.be>
2025-02-21 13:41:28 +0100

    added dot net8 project structure

---
+++ src/Games/GhostlyGame/Platforms/Windows.net8/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * The program is developed as a data collection tool in the fields of motion 
+ * analysis and physical condition.The user of the software is motivated to 
+ * complete exercises through the use of Games. This program is available as
+ * a part of the open source project OpenFeasyo found at
+ * https://github.com/openfeasyo/OpenFeasyo>.
+ * 
+ * Copyright (c) 2020 - Katarina Kostkoa
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License version 3 as published 
+ * by the Free Software Foundation. The Software Source Code is submitted 
+ * within i-DEPOT holding reference number: 122388.
+ */
+using System;
+using OpenFeasyo.Platform.Controls.Analysis;
+using OpenFeasyo.Platform.Controls.Drivers;
+
+namespace GhostlyGame
+{
+#if WINDOWS || LINUX || NET8_0
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            InputDeviceManager.Instance = new GhostlyLib.StaticDriverManager();
+            InputAnalyzerManager.Instance = new GhostlyLib.StaticAnalysisManager();
+            using (var game = new GhostlyLib.GhostlyGame())
+                game.Run();
+        }
+    }
+#endif
+}
---
+++ src/Games/GhostlyGame/Platforms/Windows.net8/StaticAnalysisManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using FeasyMotion.C3dSerializer;
+using OpenFeasyo.Platform.Controls.Analysis;
+
+namespace GhostlyLib
+{
+    public class StaticAnalysisManager : IAnalysisManager
+    {
+        private Dictionary<string, ISkeletonAnalyzer> _skeletonAnalyzers = new Dictionary<string, ISkeletonAnalyzer>();
+        private Dictionary<string, IBalanceBoardAnalyzer> _balanceBoardAnalyzers = new Dictionary<string, IBalanceBoardAnalyzer>();
+        private Dictionary<string, IAccelerometerAnalyzer> _accelerometerAnalyzers = new Dictionary<string, IAccelerometerAnalyzer>();
+        private Dictionary<string, IEmgSignalAnalyzer> _emgAnalyzers = new Dictionary<string, IEmgSignalAnalyzer>();
+
+        public StaticAnalysisManager()
+        {
+            _skeletonAnalyzers.Add("C3dSerializer.dll".ToLower(), new C3dSkeletonSerializer());
+            _balanceBoardAnalyzers.Add("C3dSerializer.dll".ToLower(), new C3dBalanceBoardSerializer());
+            _emgAnalyzers.Add("C3dSerializer.dll".ToLower(), new C3dEmgSignalSerializer());
+        }
+
+        public bool HasSkeletonAnalyzer(string fileName)
+        {
+            return _skeletonAnalyzers.ContainsKey(fileName.ToLower());
+        }
+
+        public bool HasAccelerometerAnalyzer(string fileName)
+        {
+            return _accelerometerAnalyzers.ContainsKey(fileName.ToLower());
+        }
+
+        public bool HasBalanceBoardAnalyzer(string fileName)
+        {
+            return _balanceBoardAnalyzers.ContainsKey(fileName.ToLower());
+        }
+
+        public bool HasEmgSignalAnalyzer(string fileName)
+        {
+            return _emgAnalyzers.ContainsKey(fileName.ToLower());
+        }
+
+        public ISkeletonAnalyzer GetSkeletonAnalyzer(string fileName)
+        {
+            if (_skeletonAnalyzers.ContainsKey(fileName.ToLower()))
+            {
+                return _skeletonAnalyzers[fileName.ToLower()];
+            }
+            return null;
+        }
+
+        public IAccelerometerAnalyzer GetAccelerometerAnalyzer(string fileName)
+        {
+            if (_accelerometerAnalyzers.ContainsKey(fileName.ToLower()))
+            {
+                return _accelerometerAnalyzers[fileName.ToLower()];
+            }
+            return null;
+        }
+
+        public IBalanceBoardAnalyzer GetBalanceBoardAnalyzer(string fileName)
+        {
+            if (_balanceBoardAnalyzers.ContainsKey(fileName.ToLower()))
+            {
+                return _balanceBoardAnalyzers[fileName.ToLower()];
+            }
+            return null;
+        }
+
+        public IEmgSignalAnalyzer GetEmgSignalAnalyzer(string fileName)
+        {
+            if (_emgAnalyzers.ContainsKey(fileName.ToLower()))
+            {
+                return _emgAnalyzers[fileName.ToLower()];
+            }
+            return null;
+        }
+
+        public string GetAnalyzerModuleName(ISkeletonAnalyzer analyzer)
+        {
+            foreach (string moduleName in _skeletonAnalyzers.Keys)
+            {
+                if (_skeletonAnalyzers[moduleName].GetType().Equals(analyzer.GetType()))
+                {
+                    return moduleName.ToLower();
+                }
+            }
+            throw new ApplicationException("Module is not in the loaded modules. Something went seriously wrong, please, contact the support department!");
+        }
+
+        public string GetAnalyzerModuleName(IAccelerometerAnalyzer analyzer)
+        {
+            foreach (string moduleName in _accelerometerAnalyzers.Keys)
+            {
+                if (_accelerometerAnalyzers[moduleName].GetType().Equals(analyzer.GetType()))
+                {
+                    return moduleName.ToLower();
+                }
+            }
+            throw new ApplicationException("Module is not in the loaded modules. Something went seriously wrong, please, contact the support department!");
+        }
+
+        public string GetAnalyzerModuleName(IBalanceBoardAnalyzer analyzer)
+        {
+            foreach (string moduleName in _balanceBoardAnalyzers.Keys)
+            {
+                if (_balanceBoardAnalyzers[moduleName].GetType().Equals(analyzer.GetType()))
+                {
+                    return moduleName.ToLower();
+                }
+            }
+            throw new ApplicationException("Module is not in the loaded modules. Something went seriously wrong, please, contact the support department!");
+        }
+
+        public string GetAnalyzerModuleName(IEmgSignalAnalyzer analyzer)
+        {
+            foreach (string moduleName in _emgAnalyzers.Keys)
+            {
+                if (_emgAnalyzers[moduleName].GetType().Equals(analyzer.GetType()))
+                {
+                    return moduleName.ToLower();
+                }
+            }
+            throw new ApplicationException("Module is not in the loaded modules. Something went seriously wrong, please, contact the support department!");
+        }
+    }
+}
