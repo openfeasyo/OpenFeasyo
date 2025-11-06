40e5720293e5c056d226b3e858175b6d6fc96575
kkostkova <kkostkova@etrovub.be>
2025-11-06 20:58:27 +0100

    converters

---
+++ src/Games/GhostlyGame/Platforms/maui.net9/GhostlyGame/Converters/BoolToPatientStatusConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using System.Globalization;
+
+namespace GhostlyGame.Converters
+{
+    internal class BoolToPatientStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool status)
+                return status ? "Active" : "Non-Active";
+            return "Non-Active";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Games/GhostlyGame/Platforms/maui.net9/GhostlyGame/Converters/PatientStatusToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System.Globalization;
+
+namespace GhostlyGame.Converters
+{
+    internal class PatientStatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool status)
+                return status ? Colors.Green : Colors.Red;
+
+            return Colors.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
