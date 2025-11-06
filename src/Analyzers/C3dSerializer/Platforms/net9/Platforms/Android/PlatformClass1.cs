47dbc263b2762a7a89444218359c9f01a172b53d
lomelina@gmail.com <lomelina@gmail.com>
2025-11-06 11:46:54 +0100

    Added C3dSerializer project

---
+++ src/Analyzers/C3dSerializer/Platforms/net9/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,6 @@
+﻿namespace C3dSerializer;
+
+// All the code in this file is only included on Android.
+public class PlatformClass1
+{
+}
---
+++ src/Analyzers/C3dSerializer/Platforms/net9/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace C3dSerializer
+{
+	// All the code in this file is only included on Tizen.
+	public class PlatformClass1
+	{
+	}
+}
